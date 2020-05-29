cd7b85d3aa9862a411003a1cd0931f3012931d13
Dema323 <maxime.sateur@gmail.com>
2020-05-29 13:29:30 +0200

    -Added Subtask Test

---
+++ src/test/java/domainTests/SubtaskTest.java
@@ -0,0 +1,25 @@
+package domainTests;
+
+import com.example.demo.domain.Subtask;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SubtaskTest {
+
+    @Test
+    public void setterAndGetterTest(){
+        Subtask subtask = new Subtask();
+        subtask.setId(UUID.fromString("950889d8-b49d-4ce0-bcd0-228a2186b889"));
+        subtask.setName("title");
+        subtask.setDescription("description");
+
+        assertNotNull(subtask);
+        assertEquals(UUID.fromString("950889d8-b49d-4ce0-bcd0-228a2186b889"),subtask.getId());
+        assertEquals("title",subtask.getName());
+        assertEquals("description",subtask.getDescription());
+    }
+}
